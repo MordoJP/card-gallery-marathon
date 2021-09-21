628537c02b46933e55d495adca6141f4c6f02fae
mordojp <ghtnetsgrb@mail.ru>
2021-09-21 14:47:44 +0300

    added my stylisation

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const slides = document.querySelectorAll('.slide')
+const container = document.querySelector('.background')
 
 for (const slide of slides) {
     slide.addEventListener('click', () => {
         clearActiveClasses()
 
         slide.classList.add('active')
+
+        container.style.backgroundImage = slide.style.backgroundImage
     })
 }
 
